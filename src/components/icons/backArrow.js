d436690bc09fd3360f7947b497ec99298188e6e4
chloeolson <61156007+chloeolson@users.noreply.github.com>
2024-10-11 13:44:05 +0200

    added all icons, first draft, some might look off

---
+++ src/components/icons/backArrow.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const BackArrowIcon = () => {
+  return (
+    <svg
+      width="26"
+      height="20"
+      viewBox="0 0 26 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1.57599 9.13663L14.9482 1.81103C15.6146 1.44595 16.4286 1.92818 16.4286 2.68805V4.42857V7.85714H24C24.5523 7.85714 25 8.30486 25 8.85714V11C25 11.5523 24.5523 12 24 12H16.4286V15.8571V17.5718C16.4286 18.3376 15.6032 18.8192 14.9366 18.4424L1.56439 10.8842C0.881954 10.4985 0.888494 9.51326 1.57599 9.13663Z"
+        fill="#BB65FF"
+        stroke="#BB65FF"
+        stroke-width="2"
+      />
+    </svg>
+  );
+};
+
+export default BackArrowIcon;
