279e5d03512e61d522390b229c6225bf7defc0fb
Aja <ajha@itu.dk>
2024-10-28 10:35:32 +0100

    AH-Minor changes

--- src/App.js
+++ src/App.js
@@ -22,13 +22,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <Input/>
         <ButtonPurple />
         <ButtonYellow />
         <ButtonRed />
-        
         <PopUp isVisible={isPopupVisible} onClose={togglePopup}/>
-        
         <ProfilePictureBig />
         
       </header>
