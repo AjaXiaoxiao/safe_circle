3d2ebd0ca92f99d4f407ee3c1323d618c0860f93
jolb <jolb@itu.dk>
2024-10-31 16:08:32 +0100

    sideoverview finished

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ChatOverview from "./screens/ChatOverview";
 
 function App() {
+  const ChatOverviewHeader = "Chats";
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ChatOverview />} />
+        <Route path="/" element={<ChatOverview title={ChatOverviewHeader} />} />
       </Routes>
     </Router>
   );
--- src/screens/ChatOverview.js
+++ src/screens/ChatOverview.js
@@ -1,13 +1,20 @@
 import Topbar from "../components/Topbar/Topbar";
 import styled from "styled-components";
 import Sidebar from "../components/Sidebar/Sidebar";
+import SideOverview from "../components/SideOverview";
 
-const ColumnContainer = styled.div``;
-export default function ChatOverview() {
+const ColumnContainer = styled.div`
+  display: flex;
+  width: 100vw;
+`;
+export default function ChatOverview({ title }) {
   return (
     <div>
       <Topbar />
-      <ColumnContainer />
+      <ColumnContainer>
+        <Sidebar />
+        <SideOverview title={title} />
+      </ColumnContainer>
     </div>
   );
 }
