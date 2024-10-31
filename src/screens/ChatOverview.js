9b59a9946f00efe924b5241c768381ff9437e09d
jolb <jolb@itu.dk>
2024-10-31 12:43:10 +0100

    updated ChatOverview

--- src/screens/ChatOverview.js
+++ src/screens/ChatOverview.js
@@ -2,30 +2,12 @@ import Topbar from "../components/Topbar/Topbar";
 import styled from "styled-components";
 import Sidebar from "../components/Sidebar/Sidebar";
 
-const ChatOverviewContainer = styled.div`
-  height: 100vh;
-  width: 100vw;
-`;
-
-const ColumnContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: calc(100vh - 12vh);
-`;
-
-const SidebarContainer = styled(Sidebar)`
-  width: 80px; /* Fixed width for the sidebar */
-`;
-
+const ColumnContainer = styled.div``;
 export default function ChatOverview() {
   return (
-    <ChatOverviewContainer>
+    <div>
       <Topbar />
-      <ColumnContainer>
-        <SidebarContainer>
-          <Sidebar />
-        </SidebarContainer>
-      </ColumnContainer>
-    </ChatOverviewContainer>
+      <ColumnContainer />
+    </div>
   );
 }
