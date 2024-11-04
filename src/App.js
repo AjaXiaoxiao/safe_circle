55cdf2c87df1f66eeaa091797cc7abb166208ac3
Aja <ajha@itu.dk>
2024-11-04 10:37:51 +0100

    AH-Set up connection to database

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ChatOverview from "./screens/ChatOverview";
+import { PersonComponent } from './components/PersonComponent';
+import Parse from 'parse/dist/parse.min.js';
+
+// Parse initialization configuration
+const PARSE_APPLICATION_ID = 'DTmn145degEZbGPGdfOE2zxrYRLWRG8WOO135pKw';
+const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
+const PARSE_JAVASCRIPT_KEY = '9T8j0uEAOtfZW3fdCrpvg58pdwZvwGl44qZgyCBb';
+Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+Parse.serverURL = PARSE_HOST_URL;
 
 function App() {
   const ChatOverviewHeader = "Chats";
