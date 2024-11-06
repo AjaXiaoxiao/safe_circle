ead653546629a8a7ffbc1691c377988d94ae28a8
chloeolson <61156007+chloeolson@users.noreply.github.com>
2024-11-06 09:28:25 +0100

    Added border

--- src/components/PopUps/PopUpChildOverview.js
+++ src/components/PopUps/PopUpChildOverview.js
@@ -53,7 +53,8 @@ const PopUpContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  z-index: 1000; /* Ensures the popup is on top */
+  z-index: 1000;
+  border: 1px solid #ccc;
 `;
 
 const CloseButton = styled.div`
