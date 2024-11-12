bebb7df1a6544cf991b95e2923ff1ea46d6f4535
jolb <jolb@itu.dk>
2024-11-12 12:46:28 +0100

    moved the database configuration to the .env file

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ChatOverview from "./screens/ChatOverview";
 import PopUpContactPage from "./screens/PopUpContactPage";
@@ -7,11 +8,12 @@ import LogIn from "./screens/LogIn";
 import SignUpChild from "./screens/SignUpChild";
 import SignUpParent from "./screens/SignUpParent";
 import Contacts from "./screens/Contacts";
+import { PersonComponent } from "./components/TestDatabaseComponents/PersonComponent";
 
 // Parse initialization configuration
-const PARSE_APPLICATION_ID = "DTmn145degEZbGPGdfOE2zxrYRLWRG8WOO135pKw";
-const PARSE_HOST_URL = "https://parseapi.back4app.com/";
-const PARSE_JAVASCRIPT_KEY = "9T8j0uEAOtfZW3fdCrpvg58pdwZvwGl44qZgyCBb";
+const PARSE_APPLICATION_ID = process.env.REACT_APP_PARSE_APPLICATION_ID;
+const PARSE_HOST_URL = process.env.REACT_APP_PARSE_HOST_URL;
+const PARSE_JAVASCRIPT_KEY = process.env.REACT_APP_PARSE_JAVASCRIPT_KEY;
 Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
 Parse.serverURL = PARSE_HOST_URL;
 
@@ -21,15 +23,15 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<ChatOverview title={ChatOverviewHeader} />} />
-        <Route path="/Contacts" element={<PopUpContactPage />} />
-        <Route path="/ChildOverview" element={<ChildOverviewPage/>} />
-
+        <Route path="/contacts" element={<PopUpContactPage />} />
+        <Route path="/childoverview" element={<ChildOverviewPage />} />
         <Route path="/contactspopup" element={<PopUpContactPage />} />
         <Route path="/contacts" element={<Contacts />} />
         <Route path="/login" element={<LogIn />} />
         <Route path="/signupchild" element={<SignUpChild />} />
         <Route path="/signupparent" element={<SignUpParent />} />
       </Routes>
+      <PersonComponent />
     </Router>
   );
 }
