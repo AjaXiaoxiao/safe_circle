ed51fd3e710adea0587fb47b6732025703e7ee5a
chloeolson <61156007+chloeolson@users.noreply.github.com>
2024-11-20 12:29:32 +0100

    removed unecessary styling and handled pop ups

--- src/components/SideOverview.js
+++ src/components/SideOverview.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
-import SideOverviewHeader from "./SideOverviewHeader"
+import SideOverviewHeader from "./SideOverviewHeader";
 import ContactItem from "./ContactItem";
 import PopUpAddNewContact from "./PopUps/PopUpAddNewContact";
 import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 
 const SideOverview = ({ title }) => {
-  const [isPopupVisible, setIsPopupVisible] = useState(false); 
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
   const location = useLocation();
 
   const handleOpenPopup = () => {
@@ -16,17 +16,20 @@ const SideOverview = ({ title }) => {
   };
 
   const handleClosePopup = () => {
-    setIsPopupVisible(false); 
+    setIsPopupVisible(false);
   };
 
   return (
     <OverviewContainer>
-      <SideOverviewHeader/>
-      <Separator />
-      <ItemContainer>
-        <ContactItem />
-      </ItemContainer>
-      <PopUpAddNewContact isVisible={isPopupVisible} onClose={handleClosePopup} />
+      <SideOverviewHeader onAddClick={handleOpenPopup} title={title} />
+
+      <ContactItem />
+
+      <PopUpAddNewContact
+      onClick={handleOpenPopup}
+        isVisible={isPopupVisible}
+        onClose={handleClosePopup}
+      />
     </OverviewContainer>
   );
 };
@@ -40,15 +43,4 @@ const OverviewContainer = styled.div`
   border-top-left-radius: 20px;
   margin-top: 12vh;
   z-index: 2;
-`;
-
-
-const Separator = styled.div`
-  height: 1px;
-  background-color: #ccc;
-  width: 100%;
-`;
-
-const ItemContainer = styled.div`
-  height: calc(80vs - 100px);
 `;
