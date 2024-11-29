0a81eb60190a9bb08dd44d14d3f1e5b996810100
chloeolson <61156007+chloeolson@users.noreply.github.com>
2024-11-29 11:54:14 +0100

    draft of contact pop up not showing for child unless approved.

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useLocation } from "react-router-dom";
 import Parse from "parse/dist/parse.min.js";
+import PopUp from "./PopUps/PopUp";
 import ProfilePictureSmall from "./ProfilePictures/ProfilePictureSmall";
 import PendingIcon from "./Notifications/PendingIcon";
 import colors from "../assets/colors";
@@ -100,26 +101,47 @@ const ContactList = ({ onContactClick }) => {
 
 export default ContactList;
 
+
 const ContactItem = ({
   username,
   message,
   showMessage,
   isRequest,
   onClick,
+  isApproved
 }) => {
+
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+  const handleItemClick = () => {
+    if (!isApproved) {
+      setIsPopupVisible(true);
+    } else {
+      onClick?.(); 
+    }
+  };
+
+  const handleClosePopup = () => {
+    setIsPopupVisible(false);
+  };
+
+
   return (
-    <Item onClick={onClick}>
-      <ProfileContainer>
-        <ProfilePictureSmall />
-      </ProfileContainer>
-      <TextContainer>
-        <Name>{username}</Name>
-        {showMessage && (
-          <MessageText>{message || "Hello. How are you doing.."}</MessageText>
-        )}
-        {isRequest && <PendingIcon />}
-      </TextContainer>
-    </Item>
+    <>
+      <Item onClick={handleItemClick}>
+        <ProfileContainer>
+          <ProfilePictureSmall />
+        </ProfileContainer>
+        <TextContainer>
+          <Name>{username}</Name>
+          {showMessage && (
+            <MessageText>{message || "Hello. How are you doing.."}</MessageText>
+          )}
+          {isRequest && <PendingIcon />}
+        </TextContainer>
+      </Item>
+      { isPopupVisible && <PopUp title="This contact isn't approved yet!" description=" Your guardian will need to do this for you before you can message this contact." onClose={handleClosePopup}/>}
+    </>
   );
 };
 
@@ -156,106 +178,3 @@ const Name = styled.div`
 const MessageText = styled.p`
   font-size: 0.9em;
 `;
-
-// import React, { useState, useEffect } from "react";
-// import styled from "styled-components";
-// import { useLocation } from "react-router-dom";
-// import Parse from "parse/dist/parse.min.js";
-// import ProfilePictureSmall from "./ProfilePictures/ProfilePictureSmall";
-// import PendingIcon from "./Notifications/PendingIcon";
-
-// const ContactList = () => {
-//   const [contacts, setContacts] = useState([]);
-//   const [error, setError] = useState(null);
-//   const location = useLocation();
-
-//   useEffect(() => {
-//     const fetchContacts = async () => {
-//       try {
-//         const query = new Parse.Query("ContactList");
-//         const results = await query.find();
-//         const fetchedContacts = results.map((contact) => ({
-//           username: contact.get("username"),
-//         }));
-//         setContacts(fetchedContacts);
-//       } catch (error) {
-//         console.error("Error fetching contacts:", error);
-//         setError("Failed to fetch contact.");
-//       }
-//     };
-
-//     fetchContacts();
-//   }, []);
-
-//   const showMessage = location.pathname === "/";
-//   const isRequest = location.pathname === "/ChildOverview";
-
-//   return (
-//     <div>
-//     {!error && contacts.length === 0 && <p>Loading...</p>}
-//     {!error &&
-//       contacts.length > 0 &&
-//       contacts.map((contact, index) => (
-//         <ContactItem
-//           key={index}
-//           username={contact.username}
-//           message={contact.message}
-//           showMessage={showMessage}
-//           isRequest={isRequest}
-
-//         />
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default ContactList;
-
-// const ContactItem = ({ username, message, showMessage, isRequest }) => {
-//   return (
-//     <Item>
-//       <ProfileContainer>
-//         <ProfilePictureSmall />
-//       </ProfileContainer>
-//       <TextContainer>
-//         <Name>{username}</Name>
-//         {showMessage && <MessageText>{message || "Hello. How are you doing.."}</MessageText>}
-//         {isRequest &&  <PendingIcon/>}
-//       </TextContainer>
-//     </Item>
-//   );
-// };
-
-// const Item = styled.div`
-//   height: 110px;
-//   width: 100%;
-//   background-color: #ffffff;
-//   border-top: 1px solid #ccc;
-//   border-bottom: 1px solid #ccc;
-//   display: flex;
-//   align-items: center;
-// `;
-
-// const ProfileContainer = styled.div`
-//   display: flex;
-//   align-items: center;
-//   margin-left: 20px;
-// `;
-
-// const TextContainer = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   text-align: left;
-//   margin-left: 25px;
-//   margin-top: 10px;
-// `;
-
-// const Name = styled.div`
-//   font-size: 1.2em;
-//   font-weight: bold;
-// `;
-
-// const MessageText = styled.p`
-//   font-size: 0.9em;
-// `;
