01ef7b633dc0d61929721deefc0133cd33514ce9
JohannaBerthelsen <johannalberthelsen@gmail.com>
2025-01-14 15:39:32 +0100

    Comments in index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import App from "./App";
 //this we have not used I think.
 import "bootstrap/dist/css/bootstrap.min.css";
 
+//explains where to render the react application inside the root div in the index.html
 const root = ReactDOM.createRoot(document.getElementById("root"));
+//decides what should be rendered, and that it should be in StrictMode, helping you to debug
 root.render(
   <React.StrictMode>
     <App />
